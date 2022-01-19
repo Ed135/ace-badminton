cb692665f29b1ff610a4f34f1b66a375d47b6e59
Ed Bieda <edward.bieda@gmail.com>
2022-01-19 14:09:48 +0000

    changing fav icon and a couple other changes to restrictions

---
+++ DEVELOPER_NOTES.md
@@ -0,0 +1,16 @@
+## DEVELOP NOTES
+
+### MAIL CHIMP POPOUT
+
+POST api for mail chimp users to subscribe
+https://mailchimp.com/developer/marketing/api/list-merges/
+
+We want to submit this out with the email and name details
+
+making sure they are subscription 
+
+marketing permissions are a toggle on subscription
+
+MUIv5 component for popout
+https://mui.com/components/dialogs/
+https://dev.to/gedalyakrycer/create-an-email-list-with-react-mailchimp-965
--- src/components/MobileAppBar.tsx
+++ src/components/MobileAppBar.tsx
@@ -4,8 +4,8 @@ import CssBaseline from '@mui/material/CssBaseline';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import HomeIcon from '@mui/icons-material/Home';
-import PeopleIcon from '@mui/icons-material/People';
-import GroupsIcon from '@mui/icons-material/Groups';
+// import PeopleIcon from '@mui/icons-material/People';
+// import GroupsIcon from '@mui/icons-material/Groups';
 import HelpCenterIcon from '@mui/icons-material/HelpCenter';
 import Paper from '@mui/material/Paper';
 import Link from '../components/Link';
@@ -28,8 +28,8 @@ export default function FixedBottomNavigation(props: { children: any}) {
           }}
         >
           <BottomNavigationAction label="Home" icon={<HomeIcon />} component={Link} href="/" />
-          <BottomNavigationAction label="Org" icon={<PeopleIcon />} component={Link} href="/organisation" />
-          <BottomNavigationAction label="Teams" icon={<GroupsIcon />} component={Link} href="/teams" />
+          {/* <BottomNavigationAction label="Org" icon={<PeopleIcon />} component={Link} href="/organisation" /> */}
+          {/* <BottomNavigationAction label="Teams" icon={<GroupsIcon />} component={Link} href="/teams" /> */}
           <BottomNavigationAction label="Info" icon={<HelpCenterIcon />} component={Link} href="/info" />
         </BottomNavigation>
       </Paper>
