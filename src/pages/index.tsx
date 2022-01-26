bc4e7179de024bbe683a90a594c60348e72dc7f7
Ed Bieda <edward.bieda@gmail.com>
2022-01-26 15:57:01 +0000

    adding in more info that is accurate and 2nd page details

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,17 +22,26 @@ const Home: NextPage = () => {
         <Box component="img" alt="ACE Badminton Club" sx={{ display: { xs: 'none', sm: 'flex' }, width: '60%'}} src="/AceLogoWhite.svg" />
         <Box component="img" alt="ACE Badminton Club" sx={{ display: { xs: 'flex', sm: 'none' }, width: '100%'}} src="/AceLogoWhite.svg" />
         <Divider />
-        <Paper sx={{ padding: 3, margin: 4 }}>
+        <Paper sx={{ padding: 3, margin: 3 }}>
           <Typography component="p" textAlign="center" gutterBottom>
             Bournemouth run badminton club. We play casually on Wednesdays, for all levels, Beginner through to Expert!
           </Typography>
           <Typography component="p" textAlign="center" gutterBottom>
-            <b>Wednesday - 7-9pm - Harewood School</b>
+            <b>Wednesday - 7-9pm - Avonbourne Academy (BH7 6NY)</b>
           </Typography>
           <Typography component="p" textAlign="center" gutterBottom>
             Turning up on the night - <b>£5.50</b> - Memebership <b>£42</b> per quarter <i>(Jan - March, ...)</i>
           </Typography>
-        </Paper> 
+        </Paper>
+        <Paper sx={{ padding: 3, margin: 3 }}>
+          <Typography component="p" textAlign="center" gutterBottom>
+            <b>How to Join!</b>
+          </Typography>
+          <Typography component="p" textAlign="center" gutterBottom>
+            There is no membership assoicated with turning up to play. To join come along to any of our Wednesday sessions, or reach out to us.
+            <br />Contact details can be found on the information page!
+          </Typography>
+        </Paper>
       </Box>
     </Container>
   );
--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -3,6 +3,7 @@ import type { NextPage } from 'next';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 
 const Info: NextPage = () => {
   return (
@@ -18,8 +19,32 @@ const Info: NextPage = () => {
         }}
       >
         <Typography variant="h3" component="h3" textAlign="center" gutterBottom>
-          Additional Information
+          Additional Info
         </Typography>
+        <Paper sx={{ padding: 3, margin: 3 }}>
+          <Typography component="p" textAlign="center" gutterBottom>
+            <b>Contact Details</b>
+          </Typography>
+          <Typography component="p" textAlign="center" gutterBottom>
+            Ace Badminton Email: <b><a href="mailto:someone@yoursite.com" target="_blank" rel="noreferrer">acebadminton@dummy.com</a></b> 
+          </Typography>
+        </Paper>
+        <Paper sx={{ padding: 3, margin: 3 }}>
+          <Typography component="p" textAlign="center" gutterBottom>
+            <b>Where we play?</b>
+          </Typography>
+          <Typography component="p" textAlign="center" gutterBottom>
+            Roys Moore Sports Hall<br />Bournemouth BH7 6NY 
+          </Typography>
+        </Paper>
+        <Paper sx={{ padding: 3, margin: 3 }}>
+          <Typography component="p" textAlign="center" gutterBottom>
+            <b>Payment Details</b>
+          </Typography>
+          <Typography component="p" textAlign="center" gutterBottom>
+            <b>Sort Code</b>: 12 34 56<br /><b>Account Number</b>: 12345678 
+          </Typography>
+        </Paper>
       </Box>
     </Container>
   );
